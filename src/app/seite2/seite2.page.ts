214fb1ed6c6cf7456a9d39fbd2d3f9fc6472f3d0
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-12-07 22:33:21 +0100

    Zweite Seite mit Grid-Layout dazu, zwei Zeilen mit drei Cards.

---
+++ src/app/seite2/seite2.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seite2',
+  templateUrl: './seite2.page.html',
+  styleUrls: ['./seite2.page.scss'],
+})
+export class Seite2Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
