6372379c9dfb4cc5067650fc3ce314526e09c254
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-12-07 22:17:01 +0100

    Auf Seite 1 jetzt insgesamt vier Karten

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
+
+/**
+ * Für weitere Möglichkeiten mit dem Element `ion-card` siehe:
+ * https://www.positronx.io/ionic-card-component-example/
+ */ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
 
 }
